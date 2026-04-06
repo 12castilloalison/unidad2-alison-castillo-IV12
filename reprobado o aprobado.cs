c4164a40450a27724b891e6b3e8740358cc6b96e
12castilloalison <castilloalison16db@gmail.com>
2026-04-06 17:32:53 -0600

    Add files via upload

---
+++ reprobado o aprobado.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("ALUMNO APROBADO O REPROBADO");
+        Console.WriteLine("COLOQUE SU NOTA");
+        double a;
+        a = double.Parse(Console.ReadLine());
+        if (a <= 70)
+            Console.WriteLine("ESTA REPROBADO");
+        else
+            Console.WriteLine("ESTA APROBADO");
+
+
+    }
+}
